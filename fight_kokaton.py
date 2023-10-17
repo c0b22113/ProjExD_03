dc4e4b95205ca33d7e0a9d07bc8293b25326e595
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-17 13:49:29 +0900

    練習問題1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -42,10 +42,10 @@ class Bird:
         """
         self.img = pg.transform.flip(  # 左右反転
             pg.transform.rotozoom(  # 2倍に拡大
-                pg.image.load(f"ex03/fig/{num}.png"), 
-                0, 
-                2.0), 
-            True, 
+                pg.image.load(f"ex03/fig/{num}.png"),
+                0,
+                2.0),
+            True,
             False
         )
         self.rct = self.img.get_rect()
@@ -93,12 +93,15 @@ class Bomb:
         self.rct = self.img.get_rect()
         self.rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
         self.vx, self.vy = +5, +5
+        self.visible = True
 
     def update(self, screen: pg.Surface):
         """
         爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
         引数 screen：画面Surface
         """
+        if not self.visible:
+            return
         yoko, tate = check_bound(self.rct)
         if not yoko:
             self.vx *= -1
@@ -107,13 +110,33 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+    def remove(self):
+        self.visible = False  # 爆弾を非表示にする
+
+
+class Beam:
+    """
+    ビームに関するクラス
+    """
+    def __init__(self, bird_rct: pg.Rect):
+        self.img = pg.image.load("ex03/fig/beam.png")
+        self.rct = self.img.get_rect()
+        self.rct.midleft = bird_rct.midright
+        self.vx = 5
+        self.vy = 0
+
+    def update(self, screen: pg.Surface):
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
+
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))    
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex03/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
     bomb = Bomb((255, 0, 0), 10)
+    beams = []
 
     clock = pg.time.Clock()
     tmr = 0
@@ -121,9 +144,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    beams.append(Beam(bird.rct))
         screen.blit(bg_img, [0, 0])
-        
+
+        for beam in beams[:]:
+            beam.update(screen)
+            if beam.rct.colliderect(bomb.rct):
+                beams.remove(beam)
+                bomb.remove()
+
         if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
             bird.change_img(8, screen)
